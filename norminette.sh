dca3e5de3ef7e54a61b9c715a453beeba0da1d5a
ys4ber <youss42@proton.me>
2024-02-27 23:28:58 +0100

    script to install norminette !

---
+++ norminette.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! command -v python3 &> /dev/null
+then
+    echo "ERROR : Python 3 could not be found"
+    exit
+fi
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 could not be found, installing..."
+    sudo apt install python3-pip
+fi
+
+echo "Installing Norminette..."
+pip3 install --user norminette
+
+USER_BASE_BIN=$(python3 -m site --user-base)/bin
+if ! echo $PATH | grep -q $USER_BASE_BIN
+then
+    echo "Adding Norminette to PATH..."
+    echo "export PATH=\"$PATH:$USER_BASE_BIN\"" >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+echo "Installation complete. You can now use Norminette!"
+
+echo "help : norminette -h"
+echo "usage : norminette <file.c> OR"
+echo "usage : norminette <directory> OR"
+echo "usage : norminette <file.c> <file.c> <file.c> ..."
+
+echo "enjoy!"
